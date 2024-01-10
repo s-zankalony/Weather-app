10fd65ddb3cf59d0357b9b5ac55a4c8735713b16
Sameh Zankalony <44116804+s-zankalony@users.noreply.github.com>
2024-01-11 00:02:28 +0200

    Update script.js - another update to axios deply url

--- public/script.js
+++ public/script.js
@@ -3,8 +3,8 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   //   const request = await axios.post("/api/v1", city);
-  const deployUrl = "https://weather-app-murex-six.vercel.app/"
-  const response = await axios.post(deployUrl, { city });
+  // const deployUrl = "https://weather-app-murex-six.vercel.app/"
+  const response = await axios.post(process.env.OPEN_WEATHER_URI, { city });
 
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
